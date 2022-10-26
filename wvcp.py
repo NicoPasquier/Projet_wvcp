a50721fccc13e99b21024576bb3094dfb417b10c
etud user <etud@port-dell-precision-7520-27>
2022-10-26 13:36:17 +0200

    update

--- wvcp.py
+++ wvcp.py
@@ -5,40 +5,43 @@ import wvcp_solutionPrinter
 def wvcp(name, nr_vertices, nr_edges, neighborhoods, weight, ub_colors):
     print(name + '\n')
 
-    # ajout des sommets virtuels
-    for i in range(nr_vertices):
-        neighborhoods.append([])
-        weight.append(0)
-
     model = cp_model.CpModel()
 
     x_color = {}
-    for i in range(2 * nr_vertices):
+    for i in range(nr_vertices):
         for j in range(ub_colors):
             x_color[(i, j)] = model.NewIntVar(0, 1, 'vertices%i color%f' % (i, j))
 
+    y_color = [model.NewIntVar(0, ub_colors, 'sommet%i' % i)for i in range(nr_vertices)]
     x_weight = [model.NewIntVar(0, max(weight), 'color%i' % i) for i in range(ub_colors)]
     x_score = model.NewIntVar(0, sum(weight), 'score')
 
-    for i in range(2 * nr_vertices):
+    for i in range(nr_vertices):
         model.Add((sum(x_color[i, j] for j in range(ub_colors))) == 1)
 
-    for i in range(nr_vertices, 2 * nr_vertices):
-        model.Add(x_color[i, i - nr_vertices] == 1)
+    for i in range(nr_vertices):
+        for j in range(ub_colors):
+            b = model.NewBoolVar("")
+            model.Add(x_color[i, j] == 1).OnlyEnforceIf(b)
+            model.Add(x_color[i, j] == 0).OnlyEnforceIf(b.Not())
+            model.Add(y_color[i] == j).OnlyEnforceIf(b)
+            model.Add(y_color[i] != j).OnlyEnforceIf(b.Not())
 
-    for i1 in range(2 * nr_vertices):
+    for i1 in range(nr_vertices):
         for i2 in neighborhoods[i1]:
             for j in range(ub_colors):
                 model.Add(x_color[i1, j] + x_color[i2, j] <= 1)
 
     for j in range(ub_colors):
-        model.AddMaxEquality(x_weight[j], (x_color[i, j] * weight[i] for i in range(2 * nr_vertices)))
+        model.AddMaxEquality(x_weight[j], (x_color[i, j] * weight[i] for i in range(nr_vertices)))
 
     model.Add(x_score == sum(x_weight))
     model.Minimize(x_score)
 
     solver = cp_model.CpSolver()
-    solution_printer = wvcp_solutionPrinter.SolutionPrinter(nr_vertices, nr_edges, neighborhoods, x_color, x_weight, x_score, ub_colors)
+    solution_printer = wvcp_solutionPrinter.SolutionPrinter(nr_vertices, nr_edges, neighborhoods, x_color, y_color, x_weight, x_score, ub_colors)
     solver.parameters.enumerate_all_solutions = True
-    solver.parameters.num_search_workers = 1
+    #solver.num_search_parameters = 4
+    #solver.num_search_workers = 4
+    #solver.parameters.num_search_workers = 4
     solver.Solve(model, solution_printer)
--- wvcp_solutionPrinter.py
+++ wvcp_solutionPrinter.py
@@ -4,14 +4,15 @@ import time
 
 class SolutionPrinter(cp_model.CpSolverSolutionCallback):
 
-    def __init__(self, nr_vertices, nr_edges, neighborhoods, color, weight, score, ub_color):
+    def __init__(self, nr_vertices, nr_edges, neighborhoods, x_color, y_color, x_weight, x_score, ub_color):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__nr_vertices = nr_vertices
         self.__nr_edges = nr_edges
         self.__neighborhoods = neighborhoods
-        self.__x_color = color
-        self.__x_weight = weight
-        self.__x_score = score
+        self.__x_color = x_color
+        self.__y_color = y_color
+        self.__x_weight = x_weight
+        self.__x_score = x_score
         self.__ub_color = ub_color
         self.__solution_count = 0
         self.__start_time = time.time()
@@ -28,29 +29,24 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
         self.affichage_Matrice()
         print()
         self.affichage_Sommet_Couleur()
-        self.affichage_Sommet_Virtuel()
         print()
         self.affichage_Poids_Couleur()
         print()
         print('score = %s' % self.Value(self.__x_score), end='\n\n')
 
     def affichage_Matrice(self):
-        for i in range(2 * self.__nr_vertices):
+        for i in range(self.__nr_vertices):
             for j in range(self.__ub_color):
                 print('%i' % self.Value(self.__x_color[i, j]), end=' ')
             print()
 
     def affichage_Sommet_Couleur(self):
+        """for i in range(self.__nr_vertices):
+            for j in range(self.__ub_color):
+                if self.Value(self.__x_color[i, j]) == 1:
+                    print('sommet %i = %f' % (i, j))"""
         for i in range(self.__nr_vertices):
-            for j in range(self.__ub_color):
-                if self.Value(self.__x_color[i, j]) == 1:
-                    print('sommet %i = %f' % (i, j))
-
-    def affichage_Sommet_Virtuel(self):
-        for i in range(self.__nr_vertices, 2 * self.__nr_vertices):
-            for j in range(self.__ub_color):
-                if self.Value(self.__x_color[i, j]) == 1:
-                    print('sommet virtuel %i = %f' % (i, j))
+            print('sommet %i = %f' % (i, self.Value(self.__y_color[i])))
 
     def affichage_Poids_Couleur(self):
         print('poids des couleur: [', end='')
